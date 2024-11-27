dfbfbc6979e2fb2ec10a1db7fdfc4a1803f1ac4e
aditya-samal <samaladitya2004@gmail.com>
2024-11-27 14:07:37 +0530

    File Structure Updated

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import Topnav from "./_components/Topnav";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import LeftNav from "./_components/leftnav";
 
 export const metadata: Metadata = {
   title: "Dream 11",
@@ -23,23 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.variable}>
-      <body className="bg-primary-white min-h-screen font-sans">
-        <div className="flex flex-col min-h-screen">
-          <div className="flex flex-grow">
-            <aside className="w-1/6 bg-white h-screen fixed">
-              <LeftNav />
-            </aside>
-
-            <main className="flex flex-col w-5/6 ml-[16.666%] bg-gray-100 min-h-screen">
-              <div>
-                <Topnav />
-              </div>
-
-              <div className="flex-grow p-4">{children}</div>
-            </main>
-          </div>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
